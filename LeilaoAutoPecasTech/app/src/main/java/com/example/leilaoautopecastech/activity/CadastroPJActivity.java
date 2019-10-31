d2c95814cd1b8d684e06e365ce9ae4b094c678a8
edsonrgr <digabum@gmail.com>
2019-10-31 03:08:52 -0300

    Adicionando o Projeto com slider e algumas telas

---
+++ LeilaoAutoPecasTech/app/src/main/java/com/example/leilaoautopecastech/activity/CadastroPJActivity.java
@@ -0,0 +1,16 @@
+package com.example.leilaoautopecastech.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.leilaoautopecastech.R;
+
+public class CadastroPJActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_pj);
+    }
+}
---
+++ LeilaoAutoPecasTech/app/src/main/java/com/example/leilaoautopecastech/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.leilaoautopecastech.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.leilaoautopecastech.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
