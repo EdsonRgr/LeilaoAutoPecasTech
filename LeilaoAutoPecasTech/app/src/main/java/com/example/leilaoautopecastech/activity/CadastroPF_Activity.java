22e52d2af8888e7aa589a48c03dc3de3286bb2e7
edsonrgr <digabum@gmail.com>
2019-10-31 23:55:06 -0300

    Conexão com o firebase

--- LeilaoAutoPecasTech/app/src/main/java/com/example/leilaoautopecastech/activity/CadastroPF_Activity.java
+++ LeilaoAutoPecasTech/app/src/main/java/com/example/leilaoautopecastech/activity/CadastroPF_Activity.java
@@ -1,16 +1,106 @@
 package com.example.leilaoautopecastech.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.leilaoautopecastech.R;
+import com.example.leilaoautopecastech.config.configFirebase;
+import com.example.leilaoautopecastech.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class CadastroPF_Activity extends AppCompatActivity {
+
+    private EditText campoNome, campoEmail, campoSenha;
+    private FirebaseAuth autenticacao;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_pf_);
+        campoNome = findViewById(R.id.campoNome);
+        campoEmail = findViewById(R.id.campoEmail);
+        campoSenha = findViewById(R.id.campoSenha);
+        Button btnValidarPF = (Button) findViewById(R.id.btnValidarPF);
+        btnValidarPF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String textoNome = campoNome.getText().toString();
+                String textoEmail = campoEmail.getText().toString();
+                String textoSenha = campoSenha.getText().toString();
+
+
+                if (!textoNome.isEmpty() &&
+                        !textoEmail.isEmpty() &&
+                        !textoSenha.isEmpty()) {
+
+                    Usuario usuario = new Usuario();
+                    usuario.setNome(textoNome);
+                    usuario.setEmail(textoEmail);
+                    usuario.setSenha(textoSenha);
+                    cadastrarUsuario (usuario);
+
+                } else {
+                    Toast.makeText(CadastroPF_Activity.this, "preencha todos os dados !", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
-}
+
+
+        public final void cadastrarUsuario(Usuario usuario){
+            autenticacao = configFirebase.getFirebaseAutenticacao();
+            autenticacao.createUserWithEmailAndPassword(
+                    usuario.getEmail(), usuario.getSenha()
+            ).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+
+                        Toast.makeText(CadastroPF_Activity.this,"Sucesso ao cadastrar Usuário !",
+                                Toast.LENGTH_SHORT).show();
+                        finish();
+
+                    }else{
+                        String excecao = "";
+                        try {
+                            throw task.getException();
+                        }catch (FirebaseAuthWeakPasswordException e){
+                            excecao = "Digite uma senha mais forte";
+                        }catch (FirebaseAuthInvalidCredentialsException e){
+                            excecao = "Por favor digite um email válido";
+                        }catch (FirebaseAuthUserCollisionException e){
+                            excecao = "Esta conta já foi cadastrada";
+                        }catch (Exception e){
+                            excecao="Erro ao cadastrar Usuário" + e.getMessage();
+                            e.printStackTrace();
+                        }
+                        Toast.makeText(CadastroPF_Activity.this,
+                                excecao,Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+        }
+
+
+
+
+
+
+
+    }
+
